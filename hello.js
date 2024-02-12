226aa32b8ba91dbb8e55d467ebf9ece7095f3d8e
KNGayashan <nilangagayashan18@gmail.com>
2024-02-12 15:44:45 +0530

    JSON payload added

--- hello.js
+++ hello.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req, res)=>{
-    res.send('Hello World');
+    const message = { text: 'hello World'};
+    res.json(message);
+    
 });
 
 const port = 3000;
